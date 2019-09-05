5e905e457cc3ab579f62e210547ae74bdebfb2db
jrp8297 <jrp8297@g.rit.edu>
2019-09-05 13:39:14 -0400

    First Push: 2 scenes. with the ability to swap between them. Will need to set up data flow next.

---
+++ Over Hell And Hive/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Started up");
+        Debug.Log(SceneManager.sceneCount);
+      
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchToManagement()
+    {
+
+        Debug.Log("switching to management");
+        SceneManager.LoadSceneAsync("BaseManagement");
+
+        Debug.Log(SceneManager.sceneCount);
+    }
+
+    public void SwitchToCombat()
+    {
+        SceneManager.LoadSceneAsync("CombatScene");
+        Debug.Log("switching to combat");
+    }
+
+}
